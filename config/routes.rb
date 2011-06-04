de60a381df9d971018d457584e81b0e3b70736ef
Billy H <daxtris25@gmail.com>
2011-06-04 17:32:06 -0400

    Added Layout Link

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 MyProject::Application.routes.draw do
-  get "pages/home"
+ root :to => "pages#home"
 
-  get "pages/contact"
+match '/contact', :to => 'pages#contact'
+match '/about', :to => 'pages#about'
+match '/help', :to => 'pages#help'
+  
 
-  get "pages/about"
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -54,7 +57,6 @@ MyProject::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
